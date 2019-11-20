79615b029210bb575583344dfb35e62a7ed9e9af
Marcelo <marcelojunin2010@hotmail.com>
2019-11-20 19:11:07 -0300

    Creating header component.

---
+++ src/common/template/header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default props => (
+    <header className="main-header">
+        <a href="/#/" className="logo">
+            <span className="logo-mini">
+                <b>WC</b>
+            </span>
+            <span className="logo-lg">
+                <i className="fa fa-code">
+                    <b> Wiki</b> Code
+                </i>
+            </span>
+        </a>
+        <nav className="navbar navbar-static-top">
+            <a href className="sidebar-toggle" data-toggle="offcanvas"></a>
+        </nav>
+    </header>
+);
