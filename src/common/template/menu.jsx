d850c415b23d5d2ee4a5084fb68465a0332875bf
Marcelo <marcelojunin2010@hotmail.com>
2019-11-20 19:50:49 -0300

    Creating menu tree and menu item component.

--- src/common/template/menu.jsx
+++ src/common/template/menu.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import MenuItem from './menuItem';
+import MenuTree from './menuTree';
 
 export default props => (
     <ul className="sidebar-menu">
-        
+        <MenuItem path='#' label='Dashboard' icon='dashboard'/>
+        <MenuTree label='Configurações' icon='edit'>
+            <MenuItem path='#categories' label='Categorias' icon='list-ol'/>
+        </MenuTree>
     </ul>
 );
