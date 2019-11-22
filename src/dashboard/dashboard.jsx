6e8900ba6586f6b7fca660e8940952ccd8af47ff
Marcelo <marcelojunin2010@hotmail.com>
2019-11-21 22:17:50 -0300

    Creating itens to dashboard with grid.

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
+import ValueBox from '../common/widget/valueBox';
+import Row from '../common/layout/row';
 
 
 class Dashboard extends Component {
@@ -10,7 +12,28 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0'/>
                 <Content>
-                    Dashboard
+                    <Row>
+                        <ValueBox 
+                        cols='12 4' 
+                        color='green' 
+                        icon='list-ol'
+                        value='10'
+                        text='Total de categorias'/>
+
+                        <ValueBox 
+                        cols='12 4' 
+                        color='red' 
+                        icon='list-ul'
+                        value='100'
+                        text='Total de subcategorias'/>
+
+                        <ValueBox 
+                        cols='12 4' 
+                        color='blue' 
+                        icon='align-left'
+                        value='20'
+                        text='Total de wikis'/>
+                    </Row>
                 </Content>
             </div>
         )
