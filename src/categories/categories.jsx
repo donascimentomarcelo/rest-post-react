7db96a9aa2651e514b0b8cc0e2f5d28ad3aff783
Marcelo <marcelojunin2010@hotmail.com>
2019-11-23 23:22:31 -0300

    Creating TabHeader and connecting via reducers.

--- src/categories/categories.jsx
+++ src/categories/categories.jsx
@@ -6,6 +6,7 @@ import Content from '../common/template/content';
 import Tabs from '../common/tab/tabs';
 import TabsHeader from '../common/tab/tabsHeader';
 import TabsContent from '../common/tab/tabsContent';
+import TabHeader from '../common/tab/tabHeader';
 
 export class Categories extends Component {
     render() {
@@ -15,10 +16,13 @@ export class Categories extends Component {
                 <Content>
                     <Tabs>
                         <TabsHeader>
-
+                            <TabHeader label='listar' icon='bars' target='tabList'/>
+                            <TabHeader label='incluir' icon='plus' target='tabCreate'/>
+                            <TabHeader label='alterar' icon='pencil' target='tabUpdate'/>
+                            <TabHeader label='excluir' icon='trash-o' target='tabDelete'/>
                         </TabsHeader>
                         <TabsContent>
-
+                            
                         </TabsContent>
                     </Tabs>
                 </Content>
