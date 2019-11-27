2513782dc513b12e03cfeed84e69ad5a7da686a4
Marcelo <marcelojunin2010@hotmail.com>
2019-11-26 23:20:48 -0300

    Creating an action getList and reducer.

---
+++ src/categories/categoriesActions.js
@@ -0,0 +1,10 @@
+import Axios from "axios";
+const api_url = 'http://localhost:8080/api';
+
+export function getList() {
+    const req = Axios.get(`${api_url}/categories`);
+    return {
+        type: 'CATEGORIES_FETCHED',
+        payload: request
+    }
+}
--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import DashboardReducer from "../dashboard/dashboardReducer";
 import TabReducer from "../common/tab/tabReducer";
+import CategoriesReducer from "../categories/categoriesReducer";
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
     tab: TabReducer,
+    categories: CategoriesReducer,
 });
 
 export default rootReducer;
