c599542907c2e51db5d2a9dd426e776318c5a035
Marcelo <marcelojunin2010@hotmail.com>
2019-12-03 23:41:02 -0300

    Styling field component.

--- src/categories/categoriesForm.jsx
+++ src/categories/categoriesForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form';
+import labelAndinput from '../common/form/labelAndinput';
 
 export class CategoriesForm extends Component {
     render() {
@@ -7,8 +8,18 @@ export class CategoriesForm extends Component {
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className="box-body">
-                    <Field name='name' component='input'/>
-                    <Field name='description' component='input'/>
+                    <Field 
+                        name='name' 
+                        component={labelAndinput}
+                        label="Nome"
+                        cols="12 6"
+                        placeholder="Informe o nome"/>
+                    <Field 
+                        name='description' 
+                        component={labelAndinput}
+                        label="Descrição"
+                        cols="12 6"
+                        placeholder="Informe a descrição"/>
                 </div>
                 <div className="box-footer">
                     <button type="submit" className="btn btn-primary">Enviar</button>
