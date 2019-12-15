1199e73e1648dd6caa8131a2f30aa1982d8f0bb8
Marcelo <marcelojunin2010@hotmail.com>
2019-12-14 23:34:01 -0300

    Creating method to add field and to clone a subcategory.

--- src/subcategories/subcategoriesList.jsx
+++ src/subcategories/subcategoriesList.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 import Grid from '../common/layout/grid'
-import { Field } from 'redux-form';
+import { Field, arrayInsert } from 'redux-form';
 import Input from '../common/form/input';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 export class SubcategoriesList extends Component {
+
+    add(i, item = {}) {
+        if(!this.props.readOnly) {
+            this.props.arrayInsert('categoriesForm', 'subcategories', i, item);
+        }
+    }
 
     renderRows() {
         const list = this.props.list || [];
@@ -16,7 +24,14 @@ export class SubcategoriesList extends Component {
                         placeholder='Informe o nome'
                         readOnly={this.props.readOnly}/>
                 </td>
-                <td></td>
+                <td>
+                    <button type='button'className='btn btn-success' onClick={() => this.add(i + 1)}>
+                        <i className="fa fa-plus"></i>
+                    </button>
+                    <button type='button'className='btn btn-warning' onClick={() => this.add(i + 1, item)}>
+                        <i className="fa fa-clone"></i>
+                    </button>
+                </td>
             </tr>
         ));
     }
@@ -29,7 +44,7 @@ export class SubcategoriesList extends Component {
                     <table className='table'>
                         <tr>
                             <th>Nome</th>
-                            <th>Ações</th>
+                            <th className='table-actions'>Ações</th>
                         </tr>
                     </table>
                     <tbody>
@@ -41,4 +56,5 @@ export class SubcategoriesList extends Component {
     }
 }
 
-export default SubcategoriesList
+const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert }, dispatch);
+export default connect(null, mapDispatchToProps)(SubcategoriesList);
