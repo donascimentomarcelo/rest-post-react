f8aba19c6a0e725f2231a7111c56881b7a65cf73
Marcelo <marcelojunin2010@hotmail.com>
2020-01-06 23:09:05 -0300

    Create form component to post.

--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -6,6 +6,7 @@ import Categories from '../components/categories/categories';
 import Subcategories from '../components/subcategories/subcategories';
 import SubcategoriesForm from '../components/subcategories/subcategoriesForm';
 import Posts from '../components/posts/posts';
+import PostsForm from '../components/posts/form/postsForm';
 
 export default props => (
     <Router history={hashHistory}>
@@ -15,6 +16,8 @@ export default props => (
         <Route path='/subcategories/new' component={SubcategoriesForm}/>
         <Route path='/subcategories/:id/edit' component={SubcategoriesForm}/>
         <Route path='/posts' component={Posts}/>
+        <Route path='/posts/new' component={PostsForm}/>
+        <Route path='/posts/:id/edit' component={SubcategoriesForm}/>
         <Redirect from='*' to='/'/>
     </Router>
 );
