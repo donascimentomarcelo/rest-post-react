8e7ac59b307519c13de02a4e7e03770106a25a01
Marcelo <marcelojunin2010@hotmail.com>
2020-03-28 15:09:23 -0300

    configuring bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
 import Routes from './main/routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          <Routes/>
-      </header>
+        <Routes/>
     </div>
   );
 }
