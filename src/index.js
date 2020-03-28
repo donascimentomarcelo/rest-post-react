ce4b611f1a6ccadb940eac448eb2a79fcbf2140b
Marcelo <marcelojunin2010@hotmail.com>
2020-03-27 23:44:43 -0300

    configuring index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+
+import promise from 'redux-promise';
+import multi from 'redux-multi';
+import thunk from 'redux-thunk';
+import RootReducer from './main/rootReducer';
+
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+        && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = applyMiddleware(multi, thunk, promise)(createStore)(RootReducer, devTools);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
