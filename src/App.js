27dff847266c882fc88b0e62fa4bc54fc8e2aca2
Marcelo <marcelojunin2010@hotmail.com>
2020-04-04 23:02:03 -0300

    configurando right bar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Routes from './routes/routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './layouts/header/header';
 import Sidebar from './layouts/menu/sidebar';
+import Rightbar from './layouts/bars/rightbar';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <div className="content-wrapper">
             <Routes/>
         </div>
+        <Rightbar/>
     </div>
   );
 }
