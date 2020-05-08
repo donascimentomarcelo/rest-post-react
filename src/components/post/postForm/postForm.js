895f2e34b236cce3a7a0322f7ada29de8f4b7ed5
Marcelo <marcelojunin2010@hotmail.com>
2020-05-07 23:49:49 -0300

    ajustando logica do metodo

--- src/components/post/postForm/postForm.js
+++ src/components/post/postForm/postForm.js
@@ -64,7 +64,7 @@ export class PostForm extends Component {
         }
     }
 
-    readOnly = () => !this.props.location.pathname === CONST.POST_NEW;
+    readOnly = () => this.props.location.pathname !== CONST.POST_NEW;
 
     showButtonSubmit = (pristine, submitting) => {
         if (!this.readOnly()) {
